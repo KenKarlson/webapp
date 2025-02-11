0d3a3130c5b78a6417aafc891f8fde975dbf5d86
Ken Karlson <javarecords@mail.ru>
2025-02-11 13:34:41 +0300

    Add prettier settings -

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
 export const App = () => {
-  return <div>Hello & welcome !!!</div>
-}
+  const ids = [
+    {
+      name: 'Name 0',
+      nameIds: '',
+      descriptions: 'Description name 0',
+    },
+    {
+      name: 'Name 1',
+      nameIds: '',
+      descriptions: 'Description test texxt to test text',
+    },
+    {
+      name: 'Name 2',
+      nameIds: '',
+      descriptions: 'Description name 3 to name 2',
+    },
+  ];
+  return (
+    <div>
+      <div className="title">
+        <h1>Hello & welcome !!!</h1>
+      </div>
+      <div>
+        {ids.map((id) => {
+          return (
+            <div key={id.name}>
+              <h2>{id.name}</h2>
+              <p>{id.descriptions}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
